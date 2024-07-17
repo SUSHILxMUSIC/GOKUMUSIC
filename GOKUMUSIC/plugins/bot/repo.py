8d1aa1697c16a97cce6933853d62f99e113f9e04
SUSHILxMUSIC <99750148+SUSHILxMUSIC@users.noreply.github.com>
2024-07-17 13:33:03 +0530

    Update repo.py

--- GOKUMUSIC/plugins/bot/repo.py
+++ GOKUMUSIC/plugins/bot/repo.py
@@ -47,7 +47,7 @@ async def repo(_, message):
             list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
             count += 1
 
-        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/VIPBOLTE/GOKUMUSIC.git) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/DKN_SUPPORT)
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/SUSHILxMUSIC/GOKUMUSIC.git) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/DevilMusicSupport)
 | 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
 ----------------
 {list_of_users}"""
